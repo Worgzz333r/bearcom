221eeff900062777bf0384002f08a6a1d51e63b6
Worgenzer <worgenzer@internetdevels.com>
2026-03-19 15:05:20 +0200

    Week 2: hero simplify, guided journey tabs, secondary button, form cleanup
    Hero: remove style variants, single layout (image left, text right),
    add secondary button fields, rename Product Image to Image.
    
    Guided Journey: add subtitle field, tabs JS with sliding indicator,
    replace checklist field with formatted text, orange SVG list pointers.
    
    Forms: paragraphs collapsed by default, remove unused fields from
    flexible_page form (heading, description, benefits, image, webform).

--- drupal/web/themes/custom/bearcom/js/tabs.js
+++ drupal/web/themes/custom/bearcom/js/tabs.js
@@ -1,6 +1,42 @@
 (function (Drupal, once) {
   'use strict';
 
-  // TODO: Implement.
+  Drupal.behaviors.bearcomTabs = {
+    attach: function (context) {
+      once('bearcom-tabs', '[data-tabs]', context).forEach(function (wrapper) {
+        var triggers = wrapper.querySelectorAll('[data-tab-trigger]');
+        var panels = wrapper.querySelectorAll('[data-tab-panel]');
+        var nav = wrapper.querySelector('.guided-journey__nav');
+        var tabCount = triggers.length;
+
+        function moveSlider(index) {
+          if (!nav || !tabCount) return;
+          var tabWidth = 100 / tabCount;
+          nav.style.setProperty('--slider-left', (tabWidth * index) + '%');
+          nav.style.setProperty('--slider-width', tabWidth + '%');
+        }
+
+        triggers.forEach(function (trigger) {
+          trigger.addEventListener('click', function () {
+            var index = parseInt(this.getAttribute('data-tab-trigger'));
+
+            triggers.forEach(function (t) { t.classList.remove('is-active'); });
+            panels.forEach(function (p) { p.classList.remove('is-active'); });
+
+            this.classList.add('is-active');
+            var target = wrapper.querySelector('[data-tab-panel="' + index + '"]');
+            if (target) {
+              target.classList.add('is-active');
+            }
+
+            moveSlider(index);
+          });
+        });
+
+        // Init slider on first active tab
+        moveSlider(0);
+      });
+    }
+  };
 
 })(Drupal, once);
